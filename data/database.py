c9c404010b20f7dea57d26005ad3c7a68c64c631
ReKreker <ReKreker@uwu.com>
2023-11-02 00:41:51 +0300

    Add type hints for database.py

--- data/database.py
+++ data/database.py
@@ -19,7 +19,7 @@ class Database:
         # self.logger: structlog.typing.FilteringBoundLogger = self.dp["business_logger"]
         # self.logger.debug("Connecting to PostgreSQL", db=self.db_name)
 
-    async def connect(self):
+    async def connect(self) -> None:
         try:
             self.pool = await asyncpg.create_pool(
                 database=self.db_name,
@@ -39,12 +39,12 @@ class Database:
             pass
             # self.logger.error(f"Error connecting to the database: {e}", db=self.db_name)
 
-    async def disconnect(self):
+    async def disconnect(self) -> None:
         if self.pool:
             await self.pool.close()
             # self.logger.debug("Disconnected from the database", db=self.db_name)
 
-    async def execute_query(self, query, *args):
+    async def execute_query(self, query: str, *args) -> None | list[asyncpg.Record]:
         """
         :param query: SQL request with $1, $2
         :param args: args to insert in $1, $2
@@ -62,7 +62,7 @@ class Database:
             # self.logger.error(f"Not connected to the database", db=self.db_name)
             return None
 
-    async def create_table(self, table_name, columns):
+    async def create_table(self, table_name, columns) -> None:
         """
         :param table_name: Name of the table (expectly)
         :param columns: columns in format [col_name, col_type (INTEGER, TEXT...)]
@@ -76,7 +76,7 @@ class Database:
             pass
             # self.logger.error(f"Not connected to the database", db=self.db_name)
 
-    async def insert_data(self, table_name, data):
+    async def insert_data(self, table_name, data) -> None:
         """
         :param table_name: Name of the table (expectly)
         :param data: columns in format {col_name: col_data}
@@ -92,13 +92,13 @@ class Database:
             pass
             # self.logger.error(f"Not connected to the database", db=self.db_name)
 
-    async def get_user(self, user_id):
-        return self.execute_query("SELECT * FROM member WHERE user_id=$1", (user_id,))
+    async def get_user(self, user_id) -> list[asyncpg.Record]:
+        return await self.execute_query("SELECT * FROM member WHERE user_id=$1", user_id)
 
-    async def new_user(self, user_id, username):
+    async def new_user(self, user_id, username) -> None:
         await self.insert_data("member", {"user_id": user_id, "username": username, "karmas": 0, "vacancies": 0})
 
-    async def add_karmas(self, user_id, username, karmas):
+    async def add_karmas(self, user_id, username, karmas) -> None:
         if not self.get_user(user_id):
             await self.new_user(user_id, username)
         await self.execute_query("UPDATE member SET karmas=karmas+$1 WHERE user_id=$2", (karmas, user_id))
