38ea36cc0b333a5aad95c1fee93f5fde654103dc
ReKreker <ReKreker@uwu.com>
2023-11-02 00:43:23 +0300

    Add strategy pattern for event and practice database queries

--- data/database.py
+++ data/database.py
@@ -1,14 +1,140 @@
-import asyncpg
-import orjson
-import structlog
-from aiogram import Dispatcher
+from __future__ import annotations
 
-import utils
+from abc import ABC, abstractmethod
+
+import asyncpg
+
 from data import config
 
 
+class Strategy(ABC):
+    @classmethod
+    @abstractmethod
+    def new_x_args(cls, name: str, descr: str) -> tuple:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def del_x_by_name_args(cls, name: str) -> tuple:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def del_user_x_args(cls, union_id: int) -> tuple:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def new_user_x_args(cls, user_id: int, x_id: int) -> tuple:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_all_user_x_by_user_id_args(cls, user_id: int) -> tuple:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_all_user_x_by_id_args(cls, x_id: int) -> tuple:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_all_x_args(cls) -> tuple:
+        pass
+
+    @classmethod
+    @abstractmethod
+    def get_x_by_id_args(cls, x_id: int) -> tuple:
+        pass
+
+
+class EmptyStrategy(Strategy):
+    def new_x_args(self, name: str, descr: str) -> tuple:
+        return ()
+
+    def del_x_by_name_args(self, name: str) -> tuple:
+        return ()
+
+    def del_user_x_args(self, union_id: int) -> tuple:
+        return ()
+
+    def new_user_x_args(self, user_id: int, x_id: int) -> tuple:
+        return ()
+
+    def get_all_user_x_by_user_id_args(self, user_id: int) -> tuple:
+        return ()
+
+    def get_all_user_x_by_id_args(self, x_id: int) -> tuple:
+        return ()
+
+    def get_all_x_args(self) -> tuple:
+        return ()
+
+    def get_x_by_id_args(self, x_id: int) -> tuple:
+        return ()
+
+
+class EventStrategy(Strategy):
+    def new_x_args(self, name: str, descr: str) -> tuple:
+        return "event", {"event_name": name, "description": descr}
+
+    def del_x_by_name_args(self, name: str) -> tuple:
+        return "DELETE FROM event WHERE event_name=$1", name
+
+    def del_user_x_args(self, union_id: int) -> tuple:
+        return "DELETE FROM user_event WHERE user_event_id=$1", union_id
+
+    def new_user_x_args(self, user_id: int, x_id: int) -> tuple:
+        return "user_event", {"user_id": user_id, "event_id": x_id}
+
+    def get_all_user_x_by_user_id_args(self, user_id: int) -> tuple:
+        return "SELECT event_id AS id FROM user_event WHERE user_id=$1", user_id
+
+    def get_all_user_x_by_id_args(self, x_id: int) -> tuple:
+        return "SELECT user_event_id AS union_id, event_id AS id FROM user_event WHERE event_id=$1", x_id
+
+    def get_all_x_args(self) -> tuple:
+        return ("SELECT event_id AS id, event_name AS name FROM event ORDER BY event_id DESC",)
+
+    def get_x_by_id_args(self, x_id: int) -> tuple:
+        return "SELECT event_name AS name, description AS decsr FROM event WHERE event_id=$1", x_id
+
+
+class PracticeStrategy(Strategy):
+    def new_x_args(self, name: str, descr: str) -> tuple:
+        return "practice", {"practice_name": name, "description": descr}
+
+    def del_x_by_name_args(self, name: str) -> tuple:
+        return "DELETE FROM practice WHERE practice_name=$1", name
+
+    def del_user_x_args(self, union_id: int) -> tuple:
+        return "DELETE FROM user_practice WHERE user_practice_id=$1", union_id
+
+    def new_user_x_args(self, user_id: int, x_id: int) -> tuple:
+        return "user_practice", {"user_id": user_id, "practice_id": x_id}
+
+    def get_all_user_x_by_user_id_args(self, user_id: int) -> tuple:
+        return "SELECT practice_id AS id FROM user_practice WHERE user_id=$1", user_id
+
+    def get_all_user_x_by_id_args(self, x_id: int) -> tuple:
+        return "SELECT user_practice_id AS union_id, practice_id AS id FROM user_practice WHERE practice_id=$1", x_id
+
+    def get_all_x_args(self) -> tuple:
+        return ("SELECT practice_id AS id, practice_name AS name FROM practice ORDER BY practice_id DESC",)
+
+    def get_x_by_id_args(self, x_id: int) -> tuple:
+        return "SELECT practice_name AS name, description AS decsr FROM practice WHERE practice_id=$1", x_id
+
+
+strategies = {
+    "event": EventStrategy,
+    "practice": PracticeStrategy,
+}
+
+
 class Database:
-    def __init__(self):
+    def __init__(self, strategy: str = ""):
         # self.dp = dispatcher
         self.db_name = config.PG_DATABASE
         self.user = config.PG_USER
@@ -16,6 +142,11 @@ class Database:
         self.host = config.PG_HOST
         self.port = config.PG_PORT
         self.pool = None
+        strat = strategies.get(strategy)
+        if strat is None:
+            strat = EmptyStrategy
+        self.strat = strat()
+
         # self.logger: structlog.typing.FilteringBoundLogger = self.dp["business_logger"]
         # self.logger.debug("Connecting to PostgreSQL", db=self.db_name)
 
@@ -103,17 +234,34 @@ class Database:
             await self.new_user(user_id, username)
         await self.execute_query("UPDATE member SET karmas=karmas+$1 WHERE user_id=$2", (karmas, user_id))
 
-    async def new_event(self, event_name, event_description):
-        await self.insert_data("event", {"event_name": event_name, "description": event_description})
+    async def new_x(self, name: str, descr: str) -> None:
+        args = self.strat.new_x_args(name, descr)
+        await self.insert_data(*args)
 
-    async def del_event_by_name(self, event_name):
-        await self.execute_query("DELETE FROM event WHERE event_name=$1", (event_name,))
+    async def del_x_by_name(self, name: str) -> None:
+        args = self.strat.del_x_by_name_args(name)
+        await self.execute_query(*args)
 
-    async def new_user_event(self, user_id, event_id):
-        await self.insert_data("user_event", {"user_id": user_id, "id": event_id})
+    async def del_user_x(self, union_id: int) -> None:
+        args = self.strat.del_user_x_args(union_id)
+        await self.execute_query(*args)
 
-    async def get_events(self):
-        return await self.execute_query("SELECT event_id, event_name FROM event ORDER BY event_id DESC")
+    async def new_user_x(self, user_id: int, x_id: int) -> None:
+        args = self.strat.new_user_x_args(user_id, x_id)
+        await self.insert_data(*args)
 
-    async def get_event_by_id(self, event_id):
-        return await self.execute_query("SELECT event_name, description FROM event WHERE event_id=$1", (event_id,))
+    async def get_all_user_x_by_user_id(self, user_id: int) -> list[asyncpg.Record] | None:
+        args = self.strat.get_all_user_x_by_user_id_args(user_id)
+        return await self.execute_query(*args)
+
+    async def get_all_user_x_by_id(self, x_id: int) -> list[asyncpg.Record] | None:
+        args = self.strat.get_all_user_x_by_id_args(x_id)
+        return await self.execute_query(*args)
+
+    async def get_all_x(self) -> list[asyncpg.Record]:
+        args = self.strat.get_all_x_args()
+        return await self.execute_query(*args)
+
+    async def get_x_by_id(self, x_id: int) -> list[asyncpg.Record]:
+        args = self.strat.get_x_by_id_args(x_id)
+        return await self.execute_query(*args)
