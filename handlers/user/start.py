b8d08d429113d8422e9a69837ef2febc7d0776fe
ReKreker <ReKreker@uwu.com>
2023-11-02 00:38:02 +0300

    Register user at /start

--- handlers/user/start.py
+++ handlers/user/start.py
@@ -1,18 +1,27 @@
 from aiogram import html, types
 
+from data.database import Database
+
+db = Database()
+
 
 async def start(msg: types.Message):
     if msg.from_user is None:
         return
+
+    await db.connect()
+    if not await db.get_user(msg.from_user.id):
+        await db.new_user(msg.from_user.id, msg.from_user.full_name)
+    await db.disconnect()
+
     m = [
         f'Привет, {html.quote(msg.from_user.full_name)}!',
-        'Это бот, который будет подсчитывать баллы за факультатив',
+        'Это бот, который будет подсчитывать баллы за факультатив кружка CTF',
         'Пока он находится в разработке, так что буду рад фидбеку в <a href="tg://user?id=1309263872">лс</a>',
         '',
         'Нажми /help, чтобы узнать команды бота',
     ]
     await msg.answer("\n".join(m))
-    # TODO: если пользователя не существует в БД, то создать
 
 
 async def help_msg(msg: types.Message):
